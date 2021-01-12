470fbd07686c5366fdb43f957e73a7462bd0bbc5
PollMall <16paul.popa@gmail.com>
2021-01-12 21:56:17 +0200

    Update BeeFollowPlayer

--- Assets/Scripts/BeeFollowPlayer.cs
+++ Assets/Scripts/BeeFollowPlayer.cs
@@ -5,16 +5,37 @@ using UnityEngine;
 public class BeeFollowPlayer : MonoBehaviour
 {
     private bool activated = false;
+    private float barrierDistance = 5.0f;
+    private float movementSpeed = 5.0f;
+    private GameObject player = null;
     
     // Start is called before the first frame update
     void Start()
     {
-        
+        player = GameObject.Find(("Player"));
+        if (player == null)
+        {
+            print("Player not found");
+        }
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        float distanceToPlayer = GetDistanceToPlayer();
+        if (distanceToPlayer >= barrierDistance)    //we are not to close
+        {
+            transform.position += (transform.forward * movementSpeed * Time.deltaTime);
+        }
+    }
+
+    private float GetDistanceToPlayer()
+    {
+        if (player != null)
+        {
+            return Vector3.Distance(player.transform.position, transform.position);
+        }
+        print("Player not found");
+        return 0.0f;
     }
 }
